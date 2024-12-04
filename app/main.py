d527caa92413b56bdd27fa7a744d039f19940cc0
Tareq <tareqaldhaifani@gmail.com>
2024-12-04 23:07:52 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,11 +2,15 @@ import sys
 
 
 def main():
-
     while True:
         sys.stdout.write("$ ")
+        commands_list = ['exit']
 
         command = input()
+        command_args = command.split()
+
+        if command_args[0] in commands_list and command_args[0] == 'exit' and command_args[1] == '0':
+            sys.exit(0)
 
         sys.stdout.write(f"{command}: command not found\n")
 
