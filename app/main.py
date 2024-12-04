2c8ec38e1129370241972abfe85ca42f9c1028e5
Tareq <tareqaldhaifani@gmail.com>
2024-12-04 18:13:51 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
 
-    command = input()
+    while True:
+        sys.stdout.write("$ ")
 
-    sys.stdout.write(f"{command}: command not found\n")
+        command = input()
+
+        sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
