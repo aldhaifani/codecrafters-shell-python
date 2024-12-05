375d5a151db982352c2573fe1b262ac3f2cba4e0
Tareq <tareqaldhaifani@gmail.com>
2024-12-05 06:44:45 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 import sys
-from app.utils import commands_utils
+from app.utils.commands_utils import CommandsHandler
 
 
 def main():
+    commands_handler = CommandsHandler()
+
+    commands_map = commands_handler.get_commands_map()
     while True:
         sys.stdout.write("$ ")
-        commands_map = {"echo": commands_utils.echo_cmd, "exit": commands_utils.exit_cmd}
 
         command = input()
         command_args = command.split(' ', 1)
+        if len(command_args) == 1:
+            command_args.append("")
 
         if command_args[0] in commands_map:
                 commands_map[command_args[0]](command_args[1])
--- app/utils/commands_utils.py
+++ app/utils/commands_utils.py
@@ -1,14 +1,28 @@
 import sys
 
-def exit_cmd(arg: str):
-    if arg.isdigit():
-        sys.exit(int(arg))
-    else:
-        return 0
 
-def echo_cmd(arg):
-    try:
+class CommandsHandler:
+    def __init__(self):
+        self.commands_map = {
+            "exit": self.exit_cmd,
+            "echo": self.echo_cmd,
+            "type": self.type_cmd
+        }
+
+    def get_commands_map(self):
+        return self.commands_map
+
+    def exit_cmd(self, arg: str):
+        if arg.isdigit():
+            sys.exit(int(arg))
+        elif arg == "":
+            sys.exit(0)
+
+    def echo_cmd(self, arg: str):
         sys.stdout.write(f"{arg}\n")
-        return 1
-    except Exception:
-        return 0
+
+    def type_cmd(self, arg: str):
+        if arg in self.commands_map:
+            sys.stdout.write(f"{arg} is a shell builtin\n")
+        else:
+            sys.stdout.write(f"{arg}: not found\n")
