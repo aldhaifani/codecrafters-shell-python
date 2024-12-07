88384bc7459d653b74e0fdb50a04c72b19c8f8a6
Tareq <tareqaldhaifani@gmail.com>
2024-12-07 10:25:55 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,24 +1,29 @@
+import os
 import sys
+
 from app.utils.commands_utils import CommandsHandler
 
 
-def main():
-    commands_handler = CommandsHandler()
+def main(env_path=None):
+    commands_handler = CommandsHandler(path=env_path)
 
     commands_map = commands_handler.get_commands_map()
     while True:
         sys.stdout.write("$ ")
 
         command = input()
-        command_args = command.split(' ', 1)
-        if len(command_args) == 1:
-            command_args.append("")
+        full_command = command.split(' ', 1)
+        if len(full_command) == 1:
+            full_command.append("")
 
-        if command_args[0] in commands_map:
-                commands_map[command_args[0]](command_args[1])
+        command_lower = full_command[0].lower()
+        if command_lower in commands_map:
+            commands_map[command_lower](full_command[1])
         else:
             sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
-    main()
+    path = os.environ.get("PATH")
+
+    main(env_path=path)
--- app/utils/commands_utils.py
+++ app/utils/commands_utils.py
@@ -1,8 +1,10 @@
 import sys
+import os
 
 
 class CommandsHandler:
-    def __init__(self):
+    def __init__(self, path=None):
+        self.path = path
         self.commands_map = {
             "exit": self.exit_cmd,
             "echo": self.echo_cmd,
@@ -24,5 +26,23 @@ class CommandsHandler:
     def type_cmd(self, arg: str):
         if arg in self.commands_map:
             sys.stdout.write(f"{arg} is a shell builtin\n")
+        elif found_file := find_file(arg, self.path):
+            sys.stdout.write(f"{arg} is {found_file}\n")
         else:
             sys.stdout.write(f"{arg}: not found\n")
+
+
+def find_file(file_name: str, path: str) -> str:
+    if not path:
+        return None
+
+    if ":" in path:
+        path = path.split(':')
+    else:
+        path = [].append(path)
+
+    for p in path:
+        if os.path.exists(f"{p}/{file_name}"):
+            return f"{p}/{file_name}"
+
+    return None
